5a0c910d1c7e343f0e734f8a307d05410b2a3725
md mobin <md.mobin@nagarro.com>
2024-06-02 14:24:18 +0530

    Adfded Thymleaf and actuator

--- src/main/java/com/nagp/votingservice/controller/VotingController.java
+++ src/main/java/com/nagp/votingservice/controller/VotingController.java
@@ -4,13 +4,13 @@ import com.nagp.votingservice.model.dto.CastVote;
 import com.nagp.votingservice.model.dto.VotingStatus;
 import com.nagp.votingservice.service.VotingServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+import org.springframework.ui.Model;
+@Controller
 @RequestMapping("/vote")
 public class VotingController {
 
@@ -18,12 +18,20 @@ public class VotingController {
     VotingServices votingServices;
 
     @GetMapping
-    public VotingStatus getVotingStatus(){
-        return votingServices.getVotingStatus();
+    public String getVotingStatus(Model model){
+        VotingStatus votingStatus = votingServices.getVotingStatus();
+        model.addAttribute("data", votingStatus);
+        return "voteResult";
+    }
+
+    @GetMapping("/cast")
+    public String getVotingPage(Model model) {
+        return "castVote";
     }
 
     @PostMapping
-    public String castVote(@RequestBody CastVote castvote){
-        return votingServices.castVote(castvote);
+    public String castVote(@RequestBody CastVote castvote, Model model){
+        votingServices.castVote(castvote);
+        return "castVote";
     }
 }
